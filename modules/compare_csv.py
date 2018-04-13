9cd7cc0d3288aadcb43cc6cd96ddcac21048d64f
Manuel <info@tagirijus.de>
2018-04-13 15:40:23 +0200

    compareData()

--- modules/compare_csv.py
+++ modules/compare_csv.py
@@ -3,15 +3,17 @@ from modules import csv_file
 
 def compareCSV(csv1, csv2):
     csv_a_header = csv_file.get_csv_from_file(csv1, False)[0]
-    # csv_a = csv_file.get_csv_from_file(csv1)
+    csv_a = csv_file.get_csv_from_file(csv1, False)
 
     csv_b_header = csv_file.get_csv_from_file(csv2, False)[0]
-    # csv_b = csv_file.get_csv_from_file(csv2)
+    csv_b = csv_file.get_csv_from_file(csv2, False)
 
     if not compareHeader(csv_a_header, csv_b_header):
         print('Cannot continue, due to different columns count.')
         print()
         exit()
+
+    compareData(csv_a, csv_b, csv_a_header)
 
 
 def compareHeader(csv1, csv2):
@@ -30,3 +32,22 @@ def compareHeader(csv1, csv2):
     print('   No more differences.')
     print()
     return True
+
+
+def compareData(csv1, csv2, csv_header):
+    print('Compare DATA:')
+
+    for r, row in enumerate(csv1):
+
+        for c, col in enumerate(row):
+
+            if col != csv2[r][c]:
+                print('   DIFF in Row {}, Col {} (= Column {})'.format(
+                    r, c, csv_header[c]
+                ))
+                print('      -> CSV1 = {}'.format(col))
+                print('      -> CSV2 = {}'.format(csv2[r][c]))
+                print()
+
+    print('   No more differences.')
+    print()
